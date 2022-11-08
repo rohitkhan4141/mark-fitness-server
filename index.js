59fd39a3023b7b6cb622cc6c14f5d4fe902c1a41
rohitkhan4141 <rohitkhan4141@gmail.com>
2022-11-08 12:14:48 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 // const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const app = express();
@@ -9,6 +10,16 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+//database connection
+
+const uri = `mongodb+srv://${process.env.DB_ADMIN}:${process.env.DB_PASSWORD}@cluster0.oe1j3.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 app.get("/myproducts", (req, res) => {
   res.send("hello from product");
